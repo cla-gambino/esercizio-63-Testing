c29726080143f31761b1d45fada0e621ab1713d8
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-02-12 20:06:43 +0100

    update checkResult

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.time.DayOfWeek;
+import java.time.OffsetDateTime;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static String dataString = "2023-03-01T13:00:00Z";
+
+    public static void main(String[] args) {
+        OffsetDateTime dataTime = OffsetDateTime.parse(dataString);
+
+        Result result = getResult(dataTime);
+
+        System.out.println("l'anno è: " + result.year());
+        System.out.println("il mese è: " + result.month());
+        System.out.println("il giorno del mese è: " + result.day());
+        System.out.println("il giorno della settimana è: " + result.dayOfWeek());
+    }
+
+    static Result getResult(OffsetDateTime dataTime) {
+        int year = dataTime.getYear();
+        int month = dataTime.getMonthValue();
+        int day = dataTime.getDayOfMonth();
+        DayOfWeek dayOfWeek = dataTime.getDayOfWeek();
+        Result result = new Result(year, month, day, dayOfWeek);
+        return result;
+    }
+
+    record Result(int year, int month, int day, DayOfWeek dayOfWeek) {
     }
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,4 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
-class MainTest {
-  
+import org.junit.Test;
+
+import java.time.DayOfWeek;
+import java.time.OffsetDateTime;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void checkResult() {
+        OffsetDateTime expectedDate = OffsetDateTime.parse(Main.dataString);
+        Main.Result result = Main.getResult(expectedDate);
+
+        Main.Result expectedResult = new Main.Result(2023, 03, 01, DayOfWeek.WEDNESDAY);
+
+        assertEquals(expectedResult, result);
+    }
+
 }
